a324f02b23c7e2379d93b6e70a7038959a93817d
Daniel Brakke <brakphoto@gmail.com>
2018-06-16 22:08:36 -0500

    cleaned up

--- background.js
+++ background.js
@@ -1,13 +1,3 @@
-// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     var activeTab = tabs[0];
-//     url = activeTab.url.toString()
-//     regex = /https?:\/\/www.flickr.com\/photos\/([^\/]*)\/([^\/]*)\/sizes\/o\//
-//     if (url.match(regex)) {
-//         url = url.replace(
-//             /https?:\/\/www.flickr.com\/photos\/([^\/]*)\/([^\/]*)\/sizes\/o\//,
-//             'https://www.flickr.com/photos/$1/$2/sizes/l/')
-//     }
-// });
 chrome.runtime.onMessage.addListener(function(r, s, sR) {
     url = s.url
     regex = /https?:\/\/www\.flickr\.com\/photos\/([^\/]*)\/([^\/]*)\/sizes\/o\//
@@ -16,9 +6,7 @@ chrome.runtime.onMessage.addListener(function(r, s, sR) {
             /https?:\/\/www.flickr.com\/photos\/([^\/]*)\/([^\/]*)\/sizes\/o\//,
             'https://www.flickr.com/photos/$1/$2/sizes/l/')
         chrome.tabs.query({active: true}, (tabs) => {
-            chrome.tabs.update(null, {url: url}, (tab) => { 
-                console.log(tab.status); // loading
-            })
+            chrome.tabs.update(null, {url: url})
         })
     }
 });
