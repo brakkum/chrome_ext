6bda6196ee41a099d153b4413877f8651ddcb515
Daniel Brakke <brakphoto@gmail.com>
2018-06-16 22:49:06 -0500

    added some backup just in case

--- background.js
+++ background.js
@@ -1,10 +1,18 @@
 chrome.runtime.onMessage.addListener(function(r, s, sR) {
+    has_large = r.has_large
+    has_med = r.has_med
     url = s.url
     regex = /https?:\/\/www\.flickr\.com\/photos\/([^\/]*)\/([^\/]*)\/sizes\/o\//
     if (url.match(regex)) {
+        if (has_large) {
+            size_avail = 'l'
+        } else if (has_med) {
+            size_avail = 'z'
+        } else {
+            size_avail = 'm'
+        }
         url = url.replace(
-            /https?:\/\/www.flickr.com\/photos\/([^\/]*)\/([^\/]*)\/sizes\/o\//,
-            'https://www.flickr.com/photos/$1/$2/sizes/l/')
+            regex, 'https://www.flickr.com/photos/$1/$2/sizes/' + size_avail)
         chrome.tabs.query({active: true}, (tabs) => {
             chrome.tabs.update(null, {url: url})
         })
--- flickr.js
+++ flickr.js
@@ -1,5 +1,10 @@
 url = window.location.toString()
 regex = /https?:\/\/www.flickr.com\/photos\/([^\/]*)\/([^\/]*)\/sizes\/o\//
 if (url.match(regex)) {
-    chrome.runtime.sendMessage({message: 'url_match'})
+    has_large = document.documentElement.innerHTML.includes('Large 1024')
+    has_med = document.documentElement.innerHTML.includes('Medium 640')
+    chrome.runtime.sendMessage({
+        message: 'url_match',
+        has_large: has_large,
+        has_med: has_med})
 }
