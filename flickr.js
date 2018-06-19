59ac9cfad2c7b65c4a8dd2ba4f32644121159f46
Daniel Brakke <brakphoto@gmail.com>
2018-06-19 00:06:22 -0500

    update

--- flickr.js
+++ flickr.js
@@ -1,6 +1,10 @@
 url = window.location.toString()
-regex = /https?:\/\/www.flickr.com\/photos\/([^\/]*)\/([^\/]*)\/sizes\/[okh]\//
+regex = /https?:\/\/www.flickr.com\/photos\/([^\/]*)\/([^\/]*)\/sizes\/\w+\//
 if (url.match(regex)) {
     pic = document.getElementById('allsizes-photo')
-    pic.children[0].style.width = '90%'
+    for (i = 0; i < pic.children.length; i++) {
+        if (pic.children[i].src) {
+            pic.children[i].style.width = '100%'
+        }
+    }
 }
